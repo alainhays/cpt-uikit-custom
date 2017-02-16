a5bf4dfb6a8151a141a3952baabf1c67732f2c3c
Adrian <catspajamastech@users.noreply.github.com>
2017-02-16 14:35:22 -0800

    Update cpt-uikit-custom.php

--- cpt-uikit-custom.php
+++ cpt-uikit-custom.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: UIkit
+ * Plugin Name: CPT UIkit (Custom)
  * Plugin URI: http://getuikit.com/v2/
  * Description: UIkit Wordpress plugin support for front-end styling. Checkout these guys: https://github.com/uikit/uikit
  * Version: 2.27.1.02162017
@@ -8,12 +8,10 @@
  * Author URI: https://catspajamas.tech/
  * License: MIT
  */
- 
- // register script and styles from CDN
- wp_register_script('uikit-js','https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/uikit.min.js','jquery','2.27.1','');
- wp_register_style ('uikit-css','https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css','','2.27.1','');
-
- // enqueue scripts and style
- wp_enqueue_script('jquery');
- wp_enqueue_script('uikit-js');
- wp_enqueue_style('uikit-css');
+// register script and styles from CDN
+wp_register_script('uikit-js','https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/uikit.min.js','jquery','2.27.1','');
+wp_register_style ('uikit-css','https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css','','2.27.1','');
+// enqueue scripts and style
+wp_enqueue_script('jquery');
+wp_enqueue_script('uikit-js');
+wp_enqueue_style('uikit-css');
