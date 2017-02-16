32d816e08ffd87a642ae1aa8c8e31392f4141e00
Adrian <catspajamastech@users.noreply.github.com>
2017-02-16 12:42:56 -0800

    First commit. Here's the plugin.

---
+++ cpt-uikit-custom.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: UIkit
+ * Plugin URI: http://getuikit.com
+ * Description: UIkit support for front-end styling. Checkout these guys: https://github.com/uikit/uikit
+ * Version: 2.27.1.02162017
+ * Author: Cat's Pajamas Technology
+ * Author URI: https://catspajamas.tech/
+ * License: GPL2
+ */
+ 
+ // register script and styles from CDN
+ wp_register_script('uikit-js','https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/uikit.min.js','jquery','2.27.1','');
+ wp_register_style ('uikit-css','https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css','','2.27.1','');
+
+ // enqueue scripts and style
+ wp_enqueue_script('jquery');
+ wp_enqueue_script('uikit-js');
+ wp_enqueue_style('uikit-css');
